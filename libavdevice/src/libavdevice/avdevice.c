88057db6444580aa4b8f7ce8985f01753eb2268c
helmesjo <helmesjo@gmail.com>
2024-12-19 14:01:07 +0100

    mkdir libavdevice && cd libavdevice && ln -sv ../upstream/CONTRIBUTING.md && ln -sv ../upstream/COPYING.LGPL* ./ && ln -sv ../upstream/MAINTAINERS && texi2any --plaintext ../upstream/doc/libavdevice.texi --output ./README.txt cd .. && bdep new --package -l c -t lib,license=LGPL-2.1-or-later,prefix=src,subdir=libavdevice,buildfile-in-prefix,third-party,auto-symexport libavdevice && sed -i 's/version:.*/version: 7.0.2-a.0.z/' ./libavdevice/manifest && sed -i 's/summary:.*/summary: Utility library to aid portable multimedia programming/' ./libavdevice/manifest sed -i 's|^url:.*|url: https://git.ffmpeg.org/ffmpeg.git|' ./libavdevice/manifest && sed -i 's|email:.*|email: libav-user@ffmpeg.org ; Mailing list.|' ./libavdevice/manifest

---
+++ libavdevice/src/libavdevice/avdevice.c
@@ -0,0 +1,14 @@
+#include <libavdevice/avdevice.h>
+
+#include <errno.h>
+
+int say_hello (FILE *f, const char* n)
+{
+  if (f == NULL || n == NULL || *n == '\0')
+  {
+    errno = EINVAL;
+    return -1;
+  }
+
+  return fprintf (f, "Hello, %s!\n", n);
+}
